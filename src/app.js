f762fe2f5be6c93a4638ecf02566596e0ab9ec5c
rishank14 <rishankkalra@gmail.com>
2025-10-16 21:02:32 +0530

    .

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ app.use(
    cors({
       origin: process.env.CORS_ORIGIN,
       credentials: true,
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
    })
 );
 
